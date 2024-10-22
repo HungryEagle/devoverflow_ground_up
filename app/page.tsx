28558895ba556d375afad25c3e4189f90c8de1f1
Venugopalan Iyengar <iyengarvenugopalan@gmail.com>
2024-10-22 22:10:35 +0530

    Added assets and the routing layout metadata

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Devoverflow we&apos;re coming right now!</h1>
-      <h2 className="h2-bold">Devoverflow we&apos;re coming right now!</h2>
-      <h3 className="h3-bold">Devoverflow we&apos;re coming right now!</h3>
-    </div>
-  );
-}
